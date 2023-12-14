bc0386cb208b0b620df059936be9852a4d0c6809
Mikolaj982 <Mikolaj982.hi@gmail.com>
2023-12-14 14:43:11 +0100

    Update downloadFile.ts
    changed download cv path

--- app/pages/api/downloadFile.ts
+++ app/pages/api/downloadFile.ts
@@ -3,11 +3,11 @@ import path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const filePath = path.join(process.cwd(), 'portfolio-website/public/documents/example.pdf');
+  const filePath = path.join(process.cwd(), 'portfolio-website/public/documents/Kosmala_Mikołaj_CV.pdf');
 
   try {
     const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const fileName = 'example.pdf';
+    const fileName = 'Kosmala_Mikołaj_CV.pdf';
 
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
